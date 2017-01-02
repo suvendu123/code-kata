367936491b07d24d8c079b3cd1f6be47a9b7c1c7
Suvendu <suvendu.banerjee80@gmail.com>
2017-01-03 00:55:18 +0530

    GREEN  : just test the pass , its wrong way , will refactor later.

--- code-kata/src/main/java/com/cleancode/kata/CheckOut.java
+++ code-kata/src/main/java/com/cleancode/kata/CheckOut.java
@@ -1,20 +1,34 @@
 package com.cleancode.kata;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CheckOut {
 
 	private List<Item> items;
 	private ItemService itemService;
+	private Map<String , String> rules;
 
 	public CheckOut() {
 		items = new ArrayList<Item>();
 		itemService = new ItemService();
+		rules = new HashMap<String , String>();
 	}
 
 	public Double total() {
-		return items.stream().mapToDouble(item -> item.getPrice()).sum();
+		double specialPrice =0;
+		double total =0;
+		for(Item item : items){
+			if(rules.containsKey(item.getItemCode())){
+				specialPrice = 130.00;
+			}else{
+				total+= item.getPrice();
+			}
+			
+		}
+		return specialPrice  + total;
 	}
 
 	public void scan(String itemCode) {
@@ -22,7 +36,7 @@ public class CheckOut {
 	}
 
 	public void addRule(String itemCode, String rule) {
-		
+		rules.put(itemCode, rule);
 	}
 
 }
