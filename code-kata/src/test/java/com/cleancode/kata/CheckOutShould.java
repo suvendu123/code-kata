e8dff85fc9adfccc3d15a44b5adfa2e72d6a433d
A952 <sbandhyopadhyay@in.tesco.com>
2017-01-03 11:10:45 +0530

    RED : Add more specific use case for more items.

--- code-kata/src/test/java/com/cleancode/kata/CheckOutShould.java
+++ code-kata/src/test/java/com/cleancode/kata/CheckOutShould.java
@@ -7,53 +7,66 @@ import org.junit.Test;
 
 public class CheckOutShould {
 
-	private CheckOut checkOut;
+    private CheckOut checkOut;
 
-	@Before
-	public void setUp() {
-		checkOut = new CheckOut();
-	}
+    @Before
+    public void setUp() {
+        checkOut = new CheckOut();
+    }
 
-	@Test
-	public void should_return_price_for_no_item() {
-		assertEquals(new Double(0), checkOut.total());
+    @Test
+    public void should_return_price_for_no_item() {
+        assertEquals(new Double(0), checkOut.total());
 
-	}
+    }
 
-	@Test
-	public void should_return_price_for_one_item() {
-		checkOut.scan("A");
-		assertEquals(new Double(50), checkOut.total());
+    @Test
+    public void should_return_price_for_one_item() {
+        checkOut.scan("A");
+        assertEquals(new Double(50), checkOut.total());
 
-	}
+    }
 
-	@Test
-	public void should_scan_two_item_and_return_total_price() {
-		checkOut.scan("A");
-		checkOut.scan("B");
-		assertEquals(new Double(80), checkOut.total());
+    @Test
+    public void should_scan_two_item_and_return_total_price() {
+        checkOut.scan("A");
+        checkOut.scan("B");
+        assertEquals(new Double(80), checkOut.total());
 
-	}
+    }
 
-	@Test
-	public void should_apply_rule_and_calculate_total() {
-		checkOut.addRule(new Rule("A", 3 ,130.00));
-		checkOut.scan("A");
-		checkOut.scan("A");
-		checkOut.scan("A");
-		checkOut.scan("B");
-		assertEquals(new Double(160), checkOut.total());
+    @Test
+    public void should_apply_rule_and_calculate_total() {
+        checkOut.addRule(new Rule("A", 3, 130.00));
+        checkOut.scan("A");
+        checkOut.scan("A");
+        checkOut.scan("A");
+        checkOut.scan("B");
+        assertEquals(new Double(160), checkOut.total());
 
-	}
-	@Test
-	public void should_apply_a_diffrent_rule_and_calculate_total() {
-		checkOut.addRule(new Rule("A", 3 ,130.00));
-		checkOut.addRule(new Rule("B", 2 ,45.00));
-		checkOut.scan("A");
-		checkOut.scan("B");
-		checkOut.scan("B");
-		assertEquals(new Double(95), checkOut.total());
+    }
 
-	}
+    @Test
+    public void should_apply_a_diffrent_rule_and_calculate_total() {
+        checkOut.addRule(new Rule("A", 3, 130.00));
+        checkOut.addRule(new Rule("B", 2, 45.00));
+        checkOut.scan("A");
+        checkOut.scan("B");
+        checkOut.scan("B");
+        assertEquals(new Double(95), checkOut.total());
+
+    }
+
+    @Test
+    public void should_apply_rule_for_multiple_divided_by_quantity() {
+        checkOut.addRule(new Rule("B", 2, 45.00));
+        checkOut.scan("B");
+        checkOut.scan("B");
+        checkOut.scan("B");
+        checkOut.scan("B");
+        checkOut.scan("B");
+        assertEquals(new Double(120), checkOut.total());
+
+    }
 
 }
