44a22f43d36eb9f006affee283ff9534a6d7aac3
A952 <sbandhyopadhyay@in.tesco.com>
2017-01-04 15:44:03 +0530

    REFACTOR : refactor a little

--- code-kata/src/main/java/com/cleancode/kata/promotion/CrossProductPromotion.java
+++ code-kata/src/main/java/com/cleancode/kata/promotion/CrossProductPromotion.java
@@ -1,6 +1,8 @@
 package com.cleancode.kata.promotion;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.cleancode.kata.Cart;
 import com.cleancode.kata.item.Product;
@@ -17,19 +19,29 @@ public class CrossProductPromotion implements Promotion {
 
     @Override
     public double apply(Cart cart) {
-        if (isItemPresentInCart(cart)) {
-            for (String code : productCodes) {
-                Product product = cart.getItemService().getByCode(code);
-                cart.getItemsMap().remove(product);
-            }
+        double price=0;
+        List<Product> products = getProducts(cart);
+        if(isItemPresentInCart(products , cart.getItemsMap())){
+            price = applyDiscount(products, cart.getItemsMap());   
         }
         return price;
     }
 
-    private boolean isItemPresentInCart(Cart cart) {
-        for (String code : productCodes) {
-            Product product = cart.getItemService().getByCode(code);
-            if (cart.getItemsMap().getOrDefault(product, null) == null) {
+    private double applyDiscount(List<Product> products, Map<Product, Integer> itemsMap) {
+        for (Product product : products) {
+            itemsMap.remove(product);
+        }
+        return price;
+       
+    }
+
+    private List<Product> getProducts(Cart cart) {
+        return productCodes.stream().map(code -> cart.getItemService().getByCode(code)).collect((Collectors.toList()));
+    }
+
+    private boolean isItemPresentInCart(List<Product> products, Map<Product, Integer> itemsMap) {
+        for (Product product : products) {
+            if (itemsMap.getOrDefault(product, null) == null) {
                 return false;
             }
         }
