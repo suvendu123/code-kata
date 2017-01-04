179f67f607632a7c1d26048f4cc90cf097534d35
A952 <sbandhyopadhyay@in.tesco.com>
2017-01-04 15:22:17 +0530

    GREEN : pass the test by add some production code.

--- code-kata/src/main/java/com/cleancode/kata/promotion/CrossProductPromotion.java
+++ code-kata/src/main/java/com/cleancode/kata/promotion/CrossProductPromotion.java
@@ -3,6 +3,7 @@ package com.cleancode.kata.promotion;
 import java.util.List;
 
 import com.cleancode.kata.Cart;
+import com.cleancode.kata.item.Product;
 
 public class CrossProductPromotion implements Promotion {
 
@@ -16,8 +17,23 @@ public class CrossProductPromotion implements Promotion {
 
     @Override
     public double apply(Cart cart) {
-         return price;
+        if (isItemPresentInCart(cart)) {
+            for (String code : productCodes) {
+                Product product = cart.getItemService().getByCode(code);
+                cart.getItemsMap().remove(product);
+            }
+        }
+        return price;
     }
-   
+
+    private boolean isItemPresentInCart(Cart cart) {
+        for (String code : productCodes) {
+            Product product = cart.getItemService().getByCode(code);
+            if (cart.getItemsMap().getOrDefault(product, null) == null) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
